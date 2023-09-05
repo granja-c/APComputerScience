89979bc22a0814c3675573bfa046caba3c0a00e7
Camila Granja <granja.c@stu.janesville.k12.wi.us>
2023-09-05 10:42:15 -0500

    Initial commit

---
+++ src/favClub.java
@@ -0,0 +1,5 @@
+public class favClub {
+    public static void main(String[] args){
+        System.out.println("I'm not in any currently");
+    }
+}
---
+++ src/lang52a.java
@@ -0,0 +1,16 @@
+public class lang52a {
+    public static void main(String[] args){
+        int length = 143;
+        int width = 82;
+
+        int area = length * width;
+        int perim = length * 2 + width * 2;
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perim);
+    }
+}
+/*
+Area = 11726
+Perimeter = 450
+ */
