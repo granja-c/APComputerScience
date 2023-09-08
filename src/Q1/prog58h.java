a9726a7623a24c0281a8ea205c5e73fe23e59985
granja-c <granja.c@stu.janesville.k12.wi.us>
2023-09-08 11:24:07 -0500

    Update

--- src/Q1/prog58h.java
+++ src/Q1/prog58h.java
@@ -1,7 +1,27 @@
 package Q1;
+import java.util.*;
 
 public class prog58h {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Amount saved: ");
+        double p = sc.nextDouble();
+        System.out.print("Interest rate: ");
+        double r = sc.nextDouble();
+        System.out.print("Number of times compounded per year: ");
+        double n = sc.nextDouble();
+        System.out.print("Number of days at interest: ");
+        double t = sc.nextDouble();
 
+        // double a = (0.01 * r) / n;
+        // double b = (n * t) / 365;
+        //double c = Math.pow(a, b);
+        //double interest = p * (1 + c);
+        // double a = Math.pow(((0.01 * r) / n), ((n * 1) / 365));
+        double interest = p * (1 + (Math.pow(((0.01 * r) / n), ((n * 1) / 365))));
+        double tot = interest + p;
+
+        System.out.printf("Earned: %.2f\n", interest);
+        System.out.printf("Total: %.2f\n", tot);
     }
 }
