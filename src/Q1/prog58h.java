4e8500ed9e0b80d4f49fdcbda85ddcad5095c033
granja-c <granja.c@stu.janesville.k12.wi.us>
2023-09-12 11:25:21 -0500

    Update

--- src/Q1/prog58h.java
+++ src/Q1/prog58h.java
@@ -17,11 +17,18 @@ public class prog58h {
         // double b = (n * t) / 365;
         //double c = Math.pow(a, b);
         //double interest = p * (1 + c);
-        // double a = Math.pow(((0.01 * r) / n), ((n * 1) / 365));
-        double interest = p * (1 + (Math.pow(((0.01 * r) / n), ((n * 1) / 365))));
+        double interest = p * (Math.pow(1 + ((0.01 * r) / n), ((n * t) / 365)) - 1);
         double tot = interest + p;
 
         System.out.printf("Earned: %.2f\n", interest);
         System.out.printf("Total: %.2f\n", tot);
     }
 }
+/*
+Amount saved: 5000
+Interest rate: 11.5
+Number of times compounded per year: 365
+Number of days at interest: 900
+Earned: 1638.96
+Total: 6638.96
+ */
