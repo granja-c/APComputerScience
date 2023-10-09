56b23cbcc154a8e52adad66b15461b225a4cd5ca
granja.c <granja.c@stu.janesville.k12.wi.us>
2023-10-09 10:51:42 -0500

    Update

--- src/Q1/prog82aCl/prog82aCl.java
+++ src/Q1/prog82aCl/prog82aCl.java
@@ -6,5 +6,18 @@ public class prog82aCl {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the limit: ");
         int lim = sc.nextInt();
+        System.out.print("Enter the vehicle: ");
+        int sp = sc.nextInt();
+
+        Cl82a x = new Cl82a(lim, sp);
+        x.calc();
+        double fine = x.getFine();
+
+        System.out.print("Your fine is: $" + fine);
     }
 }
+/*
+Enter the limit: 30
+Enter the vehicle: 42
+Your fine is: $80.0
+ */
