c62fc3fc562c1be5eaf6aaf96a0e177d9f53a654
granja.c <granja.c@stu.janesville.k12.wi.us>
2023-10-18 11:15:58 -0500

    Update

--- src/Q1/prog213eCl/Cl213e.java
+++ src/Q1/prog213eCl/Cl213e.java
@@ -7,7 +7,7 @@ public class Cl213e {
     private int g3;
     private int g4;
     private int g5;
-    private int tot;
+    private double tot;
     private double g1p;
     private double g2p;
     private double g3p;
@@ -32,6 +32,23 @@ public class Cl213e {
         num = n;
         tot += 1;
         if (num < 20) g1 += 1;
+        else if (num >= 20 && num <= 39) g2 += 1;
+        else if (num >= 40 && num <= 59) g3 += 1;
+        else if (num >= 60 && num <= 79) g4 += 1;
+        else g5 +=1;
+    }
 
+    public void calc(){
+        g1p = g1 / tot * 100;
+        g2p = g2 / tot * 100;
+        g3p = g3 / tot * 100;
+        g4p = g4 / tot * 100;
+        g5p = g5 / tot * 100;
+    }
+
+    public String toString(){
+        String str = String.format("<20\t%d\t%.2f\n20-39\t%d\t%.2f\n40-59\t%d\t%.2f\n60-79\t%d\t%.2f\n>79\t%d\t%.2f",
+                g1, g1p, g2, g2p, g3, g3p, g4, g4p, g5, g5p);
+        return str;
     }
 }
--- src/Q1/prog213eCl/prog213eCl.java
+++ src/Q1/prog213eCl/prog213eCl.java
@@ -8,13 +8,23 @@ public class prog213eCl {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("Langdat/prog213e.dat"));
-
+            Cl213e cl = new Cl213e();
             while (sc.hasNext()) {
-
+                int p = sc.nextInt();
+                cl.newP(p);
             }
+            cl.calc();
+            System.out.println(cl);
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
         }
     }
 }
+/*
+<20	5	21.74
+20-39	5	21.74
+40-59	8	34.78
+60-79	3	13.04
+>79	2	8.70
+ */
