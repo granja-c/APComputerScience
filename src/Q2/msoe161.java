76aae39a9692b7dcb75705e1b9e05a83af5bd68d
granja.c <granja.c@stu.janesville.k12.wi.us>
2023-11-09 11:05:22 -0600

    Update

---
+++ src/Q2/msoe161.java
@@ -0,0 +1,11 @@
+package Q2;
+import java.util.*;
+
+public class msoe161 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+
+    }
+}
