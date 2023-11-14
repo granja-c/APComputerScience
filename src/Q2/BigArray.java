9360d87e0d39f5b640349e2140d677715c68aea7
granja.c <granja.c@stu.janesville.k12.wi.us>
2023-11-14 11:23:44 -0600

    Update

--- src/Q2/BigArray.java
+++ src/Q2/BigArray.java
@@ -77,15 +77,99 @@ public class BigArray {
 
         System.out.print("Is there a 60? ");
         boolean is60 = false;
-
         for (int i:array) {
             if (i == 60) {
                 is60 = true;
             }
         }
         if (is60) System.out.println("Yes");
-        else System.out.printf("No");
+        else System.out.println("No");
 
+        boolean same = true;
+        for (int lcv = 0; lcv <array.length;lcv++) {
+            if (array[lcv] != array[array.length-1-lcv]) {
+                same = false;
+            }
+        }
+        System.out.print("Is the array a palindrome? ");
+        if (same) System.out.println("Yes");
+        else System.out.println("No");
 
+        double average = 0;
+        for (int i : array) {
+            average += i;
+        }
+        average /= array.length;
+        int c = 0;
+        for (int i : array) {
+            if (i > average) c++;
+        }
+        System.out.println("Greater than the average: " + c);
+
+        int es = 0;
+        for (int i:array) {
+            if (i % 2 == 0) {
+                es++;
+            }
+        }
+        System.out.println("# of even numbers: " + es);
+
+        int[] rev = new int[array.length];
+        for (int lcv = 0; lcv < array.length; lcv++){
+            rev[lcv] = array[array.length-1-lcv];
+        }
+        for (int i:rev) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int[] shiftArr = shiftRight(array);
+        for (int i:shiftArr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int sum =  0;
+        for (int i:array) {
+            int digsum = 0;
+            int temp = i;
+            while (temp>0) {
+                int n = temp % 10;
+                temp /= 10;
+                digsum += n;
+            }
+            sum += digsum;
+        }
+        System.out.println("Sum of all digits: " + sum);
+    }
+
+    public static int[] shiftRight(int[] arr) {
+        int temp = arr[arr.length-1];
+        int[] shifted = new int[arr.length];
+        for (int lcv = 1; lcv < arr.length; lcv++){
+            shifted[lcv] = arr[lcv-1];
+        }
+        shifted[0] = temp;
+        return shifted;
     }
 }
+/*
+34 51 30 53 80 39 23 90 81 73 90 76 47 73 80 66 38 66 88
+34 51 30 53 80 39 23 90 81 73 90 76 47 73 80 66 38 66 88
+Middle: 73
+Average: 65
+Smallest: 23	Largest: 90
+34 51 30 53 80 39 90 23 81 73 90 76 47 73 80 66 38 66 88
+34 51 30 53 80 39 90 23 81 10 90 76 47 73 80 66 38 66 88
+44 61 40 63 90 49 100 33 91 20 100 86 57 83 90 76 48 76 98
+The ousted number is: 40
+Numbers in the 50s: 57
+Multiples of 4: 44 100 20 100 76 48 76
+Is there a 60? No
+Is the array a palindrome? No
+Greater than the average: 10
+# of even numbers: 11
+98 76 48 76 90 83 57 86 100 20 91 33 100 49 90 63 5 61 44
+98 44 61 5 63 90 49 100 33 91 20 100 86 57 83 90 76 48 76
+Sum of all digits: 172
+ */
---
+++ src/Q2/msoe162.java
@@ -0,0 +1,16 @@
+package Q2;
+import java.util.*;
+
+public class msoe162 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Side A: ");
+        double sidea = sc.nextDouble();
+        System.out.print("Side B: ");
+        double sideb = sc.nextDouble();
+        System.out.print("Angle A: ");
+        double anglea = sc.nextDouble();
+        System.out.print("Angle B: ");
+        double angleb = sc.nextDouble();
+    }
+}
