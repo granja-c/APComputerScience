68765f828f2834e6f91d59204033d5bfbb2527b8
granja.c <granja.c@stu.janesville.k12.wi.us>
2023-11-30 09:17:25 -0600

    Update

--- src/Q2/Prog410t/Cl410t.java
+++ src/Q2/Prog410t/Cl410t.java
@@ -6,6 +6,7 @@ public class Cl410t {
     private int[] members;
     private int houses;
     private double average;
+    private int aacnt;
     private int[] aboveavg;
     private double percbelow;
 
@@ -15,6 +16,7 @@ public class Cl410t {
         members = m;
         houses = h;
         average = 0;
+        aacnt = 0;
         aboveavg = new int[100];
         percbelow = 0;
     }
@@ -23,6 +25,41 @@ public class Cl410t {
         for (int i : incomes) {
             average += i;
         }
-        aver
+        average /= houses;
+        for (int lcv = 0; lcv < houses; lcv++){
+            if (incomes[lcv] > average) {
+                aboveavg[aacnt] += lcv;
+                aacnt++;
+            }
+        }
+    }
+
+    public void calcBelow() {
+        int lvl = 0;
+        for (int lcv = 0; lcv < houses; lcv++){
+            lvl = 3750 + 750 * (members[lcv] - 2);
+            if (incomes[lcv] < lvl) {
+                percbelow++;
+            }
+        }
+        percbelow = percbelow / houses * 100;
+    }
+
+    public String printRes() {
+        String str = String.format("Households exceeding average income of %.2f:\nID\t\tIncome\tMembers\n", average);
+        for (int lcv = 0; lcv < aacnt; lcv++) {
+            str += ids[aboveavg[lcv]] + "\t" + incomes[aboveavg[lcv]] + "\t" + members[aboveavg[lcv]] + "\n";
+        }
+        //add percb
+        str += String.format("Percentage of households below poverty level: %.7f", percbelow);
+        return str;
+    }
+
+    public String toString() {
+        String str = "";
+        for (int lcv = 0; lcv < houses; lcv++){
+            str += ids[lcv] + "\t" + incomes[lcv] + "\t" + members[lcv] + "\n";
+        }
+        return str;
     }
 }
--- src/Q2/Prog410t/Prog410t.java
+++ src/Q2/Prog410t/Prog410t.java
@@ -18,16 +18,69 @@ public class Prog410t {
                 count++;
             }
             Scanner sc2 = new Scanner(new File("Langdat/survey2.dat"));
-            while (sc1.hasNext()) {
+            while (sc2.hasNext()) {
                 id[count] = sc2.nextInt();
                 inc[count] = sc2.nextInt();
                 ppl[count] = sc2.nextInt();
                 count++;
             }
-
+            Cl410t survey = new Cl410t(id, inc, ppl, count);
+            survey.calcAvg();
+            survey.calcBelow();
+            System.out.println("ID\t\tIncome\tMembers");
+            System.out.println(survey);
+            System.out.println(survey.printRes());
 
         } catch (IOException e) {
             System.out.println("Can't find data file/s");
         }
     }
 }
+/*
+ID		Income	Members
+2497	12500	2
+3323	13000	5
+4521	18210	4
+6789	8000	2
+5476	6000	1
+4423	16400	3
+6587	25000	4
+3221	10500	4
+5555	15000	2
+1085	19700	3
+3097	20000	8
+4480	23400	5
+2065	19700	2
+8901	13000	3
+1041	12180	4
+1062	13240	3
+1327	19800	2
+1483	22458	8
+1900	17000	2
+2112	18125	7
+2345	15623	2
+3210	3200	6
+3600	6500	5
+3601	11970	2
+4725	8900	3
+6217	10000	2
+9280	6200	1
+
+Households exceeding average income of 14281.70:
+ID		Income	Members
+4521	18210	4
+4423	16400	3
+6587	25000	4
+5555	15000	2
+1085	19700	3
+3097	20000	8
+4480	23400	5
+2065	19700	2
+1327	19800	2
+1483	22458	8
+1900	17000	2
+2112	18125	7
+2345	15623	2
+Percentage of households below poverty level: 3.7037037
+
+ */
