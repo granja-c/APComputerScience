e48f82d70f018c7c77d0c287f09f0251857c3caf
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-01-23 11:24:24 -0600

    Update

--- src/Q3/Prog465h.java
+++ src/Q3/Prog465h.java
@@ -8,12 +8,18 @@ public class Prog465h {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("Langdat/prog465h.dat"));
-
+            int cnt = 0;
             while (sc.hasNext()) {
                 int r = sc.nextInt();
                 int c = sc.nextInt();
                 int[][] mat1 = new int[r][c];
-
+                for (int row = 0; row < mat1.length; row++) {
+                    for (int col = 0; col < mat1[col].length; col++) {
+                        int num = sc.nextInt();
+                        mat1[row][col] = num;
+                        if (num > 0) cnt++;
+                    }
+                }
             }
 
 
