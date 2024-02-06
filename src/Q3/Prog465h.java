992c21c921247861d292818ee1a0884ca3c7f2d8
granja-c <granja.c@stu.janesville.k12.wi.us>
2024-02-05 18:18:45 -0600

    update

--- src/Q3/Prog465h.java
+++ src/Q3/Prog465h.java
@@ -8,21 +8,53 @@ public class Prog465h {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("Langdat/prog465h.dat"));
-            int cnt = 0;
             while (sc.hasNext()) {
                 int r = sc.nextInt();
                 int c = sc.nextInt();
                 int[][] mat1 = new int[r][c];
-                for (int row = 0; row < mat1.length; row++) {
-                    for (int col = 0; col < mat1[col].length; col++) {
+                int[][] mat2 = new int[r][3];
+                int cnt = 0;
+                for (int row = 0; row < r; row++) {
+                    for (int col = 0; col < c; col++) {
                         int num = sc.nextInt();
                         mat1[row][col] = num;
-                        if (num > 0) cnt++;
+                        if (num != 0) {
+                            mat2[cnt][0] = row+1;
+                            mat2[cnt][1] = col+1;
+                            mat2[cnt][2] = num;
+                            cnt++;
+                        }
                     }
                 }
+                for (int[] j : mat1) {
+                    for (int i : j) {
+                        System.out.print(i + " ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+
+                if (cnt*3 < r*c) {
+                    for (int row = 0; row < cnt; row++){
+                        for (int col = 0; col < 3; col++) {
+                            System.out.print(mat2[row][col] + " ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Original matrix is sparse");
+                } else if (cnt*3 == r*c) {
+                    for (int row = 0; row < cnt; row++){
+                        for (int col = 0; col < 3; col++) {
+                            System.out.print(mat2[row][col] + " ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Original matrix and Sparse matrix are equally efficient");
+                } else {
+                    System.out.println("Original matrix is abundant");
+                }
+                System.out.println();
             }
-
-
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
