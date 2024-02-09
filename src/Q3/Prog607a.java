f929ee306e6578562c2a220946020cc7c6e99359
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-02-09 11:24:26 -0600

    Update

--- src/Q3/Prog607a.java
+++ src/Q3/Prog607a.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Prog607a {
     public static void main(String[] args) {
         String months = "ABCDEFGHIJKL";
+
         String days = "QRSTUVWXYZ";
         String year = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
         Scanner sc = new Scanner(System.in);
@@ -12,13 +13,21 @@ public class Prog607a {
         System.out.println("Select an option: ");
         int choice = sc.nextInt();
         String res = "";
+        if (choice == 1) {
+            String date = sc.next();
+            System.out.println("");
+        }
         if (choice == 2) {
             String date = sc.next();
             int m = months.indexOf(date.substring(0, 1)) + 1;
-            int d = days.indexOf(date.substring(2, 3));
+            int d = 0;
             int y = year.indexOf(date.substring(date.length()-1)) + 1971;
+            if (date.substring(2, 3).equals("0")) {
+                d = days.indexOf(date.substring(3, 4)) + 1;
+            } else {
+                d = days.indexOf(date.substring(2, 3)) + 1;
+            }
             System.out.println(m + "/" + d + "/" + y);
-            // 1/12/32
         }
     }
 }
