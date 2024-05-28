542cc545f216c719401d3713090477fb0ac09547
granja-c <112727067+granja-c@users.noreply.github.com>
2024-05-28 14:05:10 -0500

    Update NeuralNetworkTest.java

--- src/Q4/Prog7301m/NeuralNetworkTest.java
+++ src/Q4/Prog7301m/NeuralNetworkTest.java
@@ -2,15 +2,16 @@ package Q4.Prog7301m;
 
 public class NeuralNetworkTest {
     public static void main(String[] args) {
-        double[][] X_train = {{0,0}};
-        double [][] y_train = {{0}};
+        double[][] X_train = {{0,0}, {0, 1}, {1, 0}, {1, 1}};
+        double [][] y_train = {{0}, {1}, {1}, {0}};
         var model = new MultiLayerPerceptron(new int[]{2, 3, 1}, 0.1, new Activations.Tanh());
 
         model.train(X_train, y_train, 1000);
         for (int i = 0; i < X_train.length; i++) {
             double res[] = model.predict(X_train[i]);
-            System.out.printf("input: %s");
+            System.out.printf("Input: %s\tOutput: %.6f\n",
+                    Arrays.toString(x), result[0]);
         }
-
+        System.out.println("Final accuracy: " + model.accuracy(X_train, y_train));
     }
 }
