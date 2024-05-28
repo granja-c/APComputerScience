99a2410c86c35edfa67745cce80fe2ad53188f8e
granja-c <112727067+granja-c@users.noreply.github.com>
2024-05-28 14:33:12 +0000

    update

---
+++ src/Q4/Prog7301m/Layer.java
@@ -0,0 +1,27 @@
+public class Layer {
+    class Neuron {
+        public double[] weights;
+        public double bias;
+        public double value;
+        public double Delta;
+        public Neuron(int pls) {
+            weights = new double[pls];
+            bias = Math.random() - 0.5;
+            value = Math.random() - 0.5;
+            Delta = Math.random() - 0.5;;
+            for (int i = 0; i < weights.length; i++) {
+                weights[i] = Math.random();
+            }
+        }
+    }
+    public Neuron[] neurons;
+    public int Size;
+
+    public Layer(int n, int s) {
+        neurons = new Neuron[n];
+        Size = n;
+        for (int i = 0; i < Size; i++) {
+            Neurons[i] = new Neuron(s);
+        }
+    }
+}
